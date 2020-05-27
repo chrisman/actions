a63d0fa98b8234c4a231e2c80dc30725e91a766d
Christopher P. Brown <christopher.p.brown@gmail.com>
2020-05-26 18:54:12 -0600

    try some artifacts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,13 @@
 
 name=$1
 
-echo "Hola $name!"
+echo "¡Hola, $name!"
+
 time=$(date)
+
 echo "::set-output name=time::$time"
 echo "::set-output name=who-was-greeted::$name"
+
+echo "name" >> name.txt
+echo $name >> name.txt
+echo "name" >> name.txt
